dfeb1f2d839f84263feca2d105f260117ab8ac5a
gRastaSsS <gerafish2398@yandex.ru>
2018-10-09 20:43:08 +0300

    Some dummy methods

--- src/main/java/com/dreamwork/art/controllers/ProjectController.java
+++ src/main/java/com/dreamwork/art/controllers/ProjectController.java
@@ -1,12 +1,27 @@
 package com.dreamwork.art.controllers;
 
+import com.dreamwork.art.model.Project;
 import com.dreamwork.art.repository.ProjectRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.domain.Pageable;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @RestController
+@RequestMapping("/projects")
 public class ProjectController {
     @Autowired
     ProjectRepository repository;
+
+    @RequestMapping(method = RequestMethod.GET)
+    public Page<Project> getAllProjects(Pageable pageable) {
+        return repository.findAll(pageable);
+    }
+
+    @RequestMapping(method = RequestMethod.POST)
+    public Project createProject(@Valid @RequestBody Project project) {
+        return repository.save(project);
+    }
 }
