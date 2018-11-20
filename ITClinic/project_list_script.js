c5c499836d7dc51952a7b42b9955ccb592bf43bf
Avdey <avdeev.1van@icloud.com>
2018-11-21 00:18:58 +0300

    prepare for demo day

--- ITClinic/project_list_script.js
+++ ITClinic/project_list_script.js
@@ -2,34 +2,37 @@
 var projects = [{
   title: 'Web-приложение для аграрного предприятия',
   status_bar: 71,
-  description: 'Тут очень краткое описание проекта, что, как, куда, зачем. Тут я напишу еще текста, чтобы как будто описание. Паровозы серии "ОВ", получившие в народе название "овечка", начали выпускать еще в царской России в 1912 году.',
+  description: 'Паровозы серии "ОВ", получившие в народе название "овечка", начали выпускать еще в царской России в 1912 году.',
   array_li: ['Web', 'Низкая сложность', 'Срочный', 'Design', '...'],
   array_li_sublist: ['tag1', 'tag2']
 }, {
   title: 'Развертка пингвиньих бегов на местности.',
   status_bar: 51,
-  description: 'Я пока не придумала, как это должно выглядеть, но хочу что-то красивое и новое. А то все эти пмовские сайты - это грустно. Нормальное число зубов у улитки - от 15 до 25 тысяч. Самый быстрый из пингвинов - субантарктический, или папуанский пингвин.',
+  description: 'Нормальное число зубов у улитки - от 15 до 25 тысяч. Самый быстрый из пингвинов - субантарктический, или папуанский пингвин.',
   array_li: ['Анализ данных', 'Низкая сложность', 'Тег 2', 'Еще один тег', '...'],
   array_li_sublist: ['tag1', 'tag2', 'tag23']
 }, {
   title: 'Вёрстка ленты Мёбиуса',
   status_bar: 88,
-  description: 'Больше бессмысленных текстов, чтобы можно было оценить, как это смотрится и что из этого получится. Хочу печеньку. Лента Мёбиуса - пример неориентируемой односторонней поверхности с одним краем в обычном трёхмерном Евклидовом пространстве.',
-  array_li: ['Анализ данных', 'Высокая сложность', 'Вёрстка', 'Design', '...'],
+  description: 'Лента Мёбиуса - пример неориентируемой односторонней поверхности с одним краем в обычном трёхмерном Евклидовом пространстве.',
+  array_li: ['Анализ данных', 'Низкая сложность', 'Вёрстка', 'Design', '...'],
   array_li_sublist: ['tag1', 'tag2', 'doka 2']
 }, {
   title: 'Расшифровка Энигмы',
   status_bar: 22,
-  description: 'А тут совершенно другое описание совершенно другого проекта. Необходим объект огромной массы, чтобы создать гравитационное поле, достаточное для того, чтобы согнуть пространство и время пополам, и показанный в фильме вариант эквивалентен сотне миллионов наших солнц.',
-  array_li: ['Анализ данных', 'Высокая сложность', 'Dota 2', 'Enigma', '...'],
+  description: 'Расшифровать машину Энигма и при этом не попасть в её "Black Hole"',
+  array_li: ['Анализ данных', 'Низкая сложность', 'Dota 2', 'Enigma', '...'],
   array_li_sublist: ['tag1', 'tag2']
 }, {
   title: 'Сдать зачет по ВЕБу',
   status_bar: 0, 
   description: 'Написать сайт, который будет удовлетворять всем требованиям преподавателя. Сайт должен быть serverless, должен содержать авторизацию. Оплата почасовая',
-  array_li: ['WEB', 'Сложная сложность', 'JS', 'Serverless'],
+  array_li: ['WEB', 'Низкая сложность', 'JS', 'Serverless'],
   array_li_sublist: []
 }];
+
+
+var button_id = 0;
 
 var makeElement = function(tagName, className, text) {
   var element = document.createElement(tagName);
@@ -51,6 +54,7 @@ var createBox = function(project) {
 
   var btn = document.createElement('div');
   var on_project_link = makeElement('a', 'on-project-page', 'На страницу проекта');
+  on_project_link.id = button_id;
   on_project_link.setAttribute('href', 'about_project.html');
   btn.appendChild(on_project_link);
   listItem.appendChild(btn);
@@ -103,6 +107,8 @@ var createBox = function(project) {
   divTags.appendChild(tags);
   listItem.appendChild(divTags);
 
+  button_id++;
+
   return listItem;
 };
 
@@ -113,7 +119,12 @@ for (var i = 0; i < projects.length; i++) {
 }
 
 var count = 0;
-
+var count_projects = 0;
+var array_tags = [];
+var hidden_count = 0;
+document.querySelectorAll('.project-box').forEach(element => {
+  array_tags.push(element);
+});
 
 document.querySelectorAll('.type-of-tags button').forEach((button) => {
   button.addEventListener('click', () => {
@@ -158,9 +169,62 @@ document.querySelectorAll('.type-of-tags button').forEach((button) => {
   });
 });
 
+// document.querySelectorAll('.type-of-tags button').forEach((button) => {
+//   button.addEventListener('click', () => {
+//     hidden_count = 0;
+//     if (count < 3) {
+//       var reset = document.querySelector('.reset');
+//       reset.style.display = 'block';
+//       //array_tags.push(button);
+
+//       count++;
+//       count_projects = 0;
+      
+//       array_tags.forEach(box => {
+//         var tags = box.querySelectorAll('.topmenu-tags li');
+//         var flag = false;
+//         tags.forEach(tag => {
+//           //alert(tag.textContent.toLowerCase() + '    ' + button.textContent.toLowerCase())
+//           if (tag.textContent.toLowerCase() == button.textContent.toLowerCase()) {
+//             flag = true;
+//             // array_tags.push(tag);
+//             count_projects++;
+//           } 
+//         });
+//         if (flag == false) {
+//           alert(box.querySelector('.title').textContent + ' index =   ' + array_tags.indexOf(box));
+//           box.style.display = 'none';
+//           var idx = array_tags.indexOf(box);
+//           array_tags.splice(idx, 1);
+//           hidden_count++;
+//         }
+//         if (array_tags.length == 0) {
+//             document.querySelector('.message').style.display = 'block';
+//         }
+        
+//         });
+//       // alert(projects.length + '  ' + hidden_count);
+//       var ull = document.querySelector('.selected-tags');
+//       var tag = document.createElement('div');
+//       tag.textContent = button.textContent;
+//       tag.style.backgroundColor = window.getComputedStyle( button , null).getPropertyValue('background-color');
+//       tag.classList.add('pressed');
+//       ull.appendChild(tag);
+//       button.disabled = true;
+
+//     } else {
+//       alert('Максимум 3 тега!!!')
+//     }
+  
+//   });
+// });
+
 var reset = document.querySelector('.reset');
 
 var Reset = function() {
+  document.querySelectorAll('.project-box').forEach(element => {
+    array_tags.push(element);
+  });
   count = 0;
   reset.style.display = 'none';
   document.querySelectorAll('.type-of-tags button').forEach(elem => {
@@ -176,9 +240,25 @@ var Reset = function() {
   document.querySelector('.message').style.display = 'none';
 
   document.querySelectorAll('.selected-tags div').forEach(element => {
-    // element.style.display = 'none';
+
     element.parentNode.removeChild(element);
   });
-}
+};
 
 reset.onclick = Reset;
+
+
+var on_project_button = document.querySelectorAll('.on-project-page');
+on_project_button.forEach((button) => {
+  button.addEventListener('click', () => {
+    var i = button.id;
+    var project = {
+      title: projects[i].title,
+      flag: projects[i].flag,
+      top_list: projects[i].array_li,
+      sub_list: projects[i].array_li_sublist,
+      difficulty: projects[i].difficulty
+    };
+
+  });
+});
