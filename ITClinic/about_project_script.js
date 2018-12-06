665eda8e44ddedce3ad7470a341ba6f641f46827
Avdey <avdeev.1van@icloud.com>
2018-12-07 01:54:32 +0300

    pre demo day commit
    added requests for graphic

--- ITClinic/about_project_script.js
+++ ITClinic/about_project_script.js
@@ -1,7 +1,6 @@
-// document.cookie = 'name=web;'
-// import 'project_list_script';
-// alert(abcd);
 
+var dates = [];
+var metrics_chart = [];
 
 function getCookie(name) {
   var matches = document.cookie.match(new RegExp(
@@ -27,31 +26,57 @@ var find = function(projects) {
 $(document).ready(function() {
   $.ajax({
     type: "GET",
-    url: "https://agile-depths-28542.herokuapp.com/api/projects?limit=5&offset=0&metrics=random", //, another", 
-    // url: "https://agile-depths-28542.herokuapp.com/api/projects/info",
-    dataType: "json",
+    url: "https://agile-depths-28542.herokuapp.com/api/public/projects?limit=10&offset=0&metrics=progress,countclosed",
     headers: {
       'Authorization': '8006590e56914f96bb2a0d67a998a234'
     },
     success: function(data) {
         project = find(data);
-        run(project);
+        var date = project.startedAt.substring(0, 10) + " " + project.startedAt.substring(11, 19);
+        var request = "https://agile-depths-28542.herokuapp.com/api/public/metrics?projectId=";
+        request += project.id;
+        request += "&from=";
+        request += date;
+        request += "&until=2019-09-17 18:47:52&interval=60";
+        // console.log(request);
+        $.ajax({
+          type: "GET",
+          
+          url: request,
+          dataType: "json",
+          headers: {
+            'Authorization': '8006590e56914f96bb2a0d67a998a234'
+          },
+          success: function(met) {
+            createDates(met);
+            createClosedCount(met);
+            console.log(met);
+            console.log(metrics_chart);
+            run(project);
+          }
+        });
     }
   });
 });
 
+var createDates = function(metrics_graph) {
+  for (let i = 0; i < metrics_graph.length; i++) {
+    var element = metrics_graph[i].timestamp.substring(0,10) + " " + metrics_graph[i].timestamp.substring(11,16);
+    dates.push(element);
+  }
+}
 
-// project = {
-//   title: 'Название',
-//   customer_link: 'ООО "Пупсень и Вупсень"',
-//   level: 'Низкая',
-//   about_project: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsum quia explicabo quidem nesciunt non ipsam sit asperiores laudantium mollitia officia. Assumenda temporibus ab ducimus, nostrum officiis sequi. Magni ex, reiciendis.',
-//   graph: 'images/q04319KqOfs.jpg',
-//   ghLink: 'хттп.гитхуб.ком',
-//   flag: 1,
-//   jobs: ['job1', 'job2', 'job3', 'job4', 'job5'],
-//   tags: ['Web', 'Низкая сложность', 'Срочный', 'Design', 'JS', 'Serverless']
-// };
+var createClosedCount = function(metrics_graph) {
+  for (let i = 0; i < metrics_graph.length; i++) {
+    var element;
+    if (metrics_graph[i].metrics[0].type == 'countclosed') {
+      element = metrics_graph[i].metrics[0].value;
+    } else {
+      element = metrics_graph[i].metrics[1].value;
+    }
+    metrics_chart.push(element);
+  }
+}
 
 var makeElement = function(tagName, className, text) {
   var element = document.createElement(tagName);
@@ -92,11 +117,12 @@ var run = function(project) {
   var graph = makeElement('div', 'graphic');
   graph.textContent += 'Зелёным цветом отмечены ожидаемые результаты, ';
   graph.textContent += 'красным - результаты реальной работы.';
+  // desc.appendChild('graph');
 
   var gh = makeElement('div', 'github', 'Github: ');
   var a_gh = document.createElement('a');
-  a_gh.href = 'https://github.com';
-  a_gh.textContent = project.githubRepo;
+  a_gh.href = 'https://github.com/' + project.githubRepo;
+  a_gh.textContent = 'Ссылка на репозиторий';
   gh.appendChild(a_gh);
   desc.appendChild(gh);
 
@@ -105,31 +131,36 @@ var run = function(project) {
 
   var jobs = document.querySelector('.list-of-jobs');
 
-  if (project.status == 3) { 
-    jobs.style.display = 'none';
+  if (project.metrics[0].value == 1) { 
     content = 'Завершён'; 
     class_status = 'finished';
-  } else if (project.status == 1 || project.status == 2) {
-    content = 'В процессе'; 
-    class_status = 'in-proccess';
-  } else { 
+  } else if (project.metrics[0].value == 0) {
     content = 'Не начат'; 
     class_status = 'in-future';
+  } else { 
+    content = 'В процессе'; 
+    class_status = 'in-proccess';
   }
   
 
   document.getElementById('status').innerHTML = content;
   document.getElementById('status').classList.add(class_status);
 
-  var color = "hard";
+  var color;
+  var project_level;
+  if (project.difficulty === 'Низкий') { 
+    color = 'easy'; 
+    project_level = 'Низкая';
+  } else if (project.difficulty === 'Средний') {
+    color = 'medium';
+    project_level = 'Средняя';
+  } else { 
+    color = "hard";
+    project_level = 'Высокая';
+  }
 
-  document.getElementById('level').innerHTML = 'Я не нашел такого в описании проекта(((((';
+  document.getElementById('level').innerHTML = project_level;
   document.getElementById('level').classList.add(color);
-
-  var list = document.querySelector('.list');
-  var listItem = document.createElement('li');
-  listItem.textContent = 'Откуда брать список вакансий?';
-  list.appendChild(listItem);
   
   var tags = document.querySelector('.tags');
   if (project.tags.length == 0) {
@@ -145,10 +176,9 @@ var run = function(project) {
   }
 
   var chart = new Chartist.Line('.ct-chart', {
-        labels: ['Week 1', '' ,'Week 2', '', 'Week 3', '', 'Week4 4', '', 'Week 5', ],
+        labels: dates,
         series: [
-          [20, 18, 21, 17, 15, 12, 9, 5, 0],
-          [20, 17, 15, 13, 10, 8, 5, 2, 0]
+          metrics_chart
         ]
       }, {
         low: 0
@@ -271,214 +301,3 @@ var run = function(project) {
 
 };
 
-
-
-
-
-
-//   var graph = makeElement('div', 'graphic');
-//   graph.textContent += 'Зелёным цветом отмечены ожидаемые результаты, ';
-//   graph.textContent += 'красным - результаты реальной работы.';
-
-//   desc.insertBefore(graph, graphic);
-//   // new Chartist.Line('.chart1', {
-//   //   labels: ['Week 1', 'Week 2', 'Week 3', 'Week4 4', 'Week 5'],
-//   //       series: [
-//   //           [20, 15, 10, 5, 0],
-//   //           [20, 14, 16, 9, 0],
-//   //           // [0, 15, 40, 60, 90]
-//   //       ]
-//   //   }, {
-//   //       fullWidth: true,
-//   //       chartPadding: {n
-//   //           right: 50
-//   //       }
-//   // });
-
-//   var gh = makeElement('div', 'github', 'Github: ');
-//   var a_gh = document.createElement('a');
-//   a_gh.href = 'https://github.com';
-//   a_gh.textContent = project.ghLink;
-//   gh.appendChild(a_gh);
-//   desc.appendChild(gh);
-
-
-//   var content;
-//   var class_status;
-
-//   if (project.flag === 0) { 
-//     content = 'Завершён'; 
-//     class_status = 'finished';
-//   } else if (project.flag === 1) {
-//     content = 'В процессе'; 
-//     class_status = 'in-proccess';
-//   } else { 
-//     content = 'Не начат'; 
-//     class_status = 'in-future';
-//   }
-
-//   document.getElementById('status').innerHTML = content;
-//   document.getElementById('status').classList.add(class_status);
-
-//   var color;
-//   if (project.level === 'Низкая') { 
-//     color = 'easy'; 
-//   } else if (project.level === 'Средняя') {
-//     color = 'medium';
-//   } else { 
-//     color = "hard";
-//   }
-
-//   document.getElementById('level').innerHTML = project.level;
-//   document.getElementById('level').classList.add(color);
-
-//   var jobs = document.querySelector('.list-of-jobs');
-//   if (project.flag == 0) {
-//     jobs.style.display = 'none';
-//   } else {
-//     var list = document.querySelector('.list');
-//     for (var i = 0; i < project.jobs.length; i++) {
-//       var listItem = document.createElement('li');
-//       listItem.textContent = '- ' + project.jobs[i];
-//       list.appendChild(listItem);
-//     }
-//   }
-
-//   var tags = document.querySelector('.tags');
-//   for (var i = 0; i < project.tags.length; i++) {
-//     var litem = document.createElement('li');
-//     litem.textContent = project.tags[i];
-//     tags.appendChild(litem);
-//   }
-
-//   var chart = new Chartist.Line('.ct-chart', {
-//     labels: ['Week 1', '' ,'Week 2', '', 'Week 3', '', 'Week4 4', '', 'Week 5', ],
-//     series: [
-//       [20, 18, 21, 17, 15, 12, 9, 5, 0],
-//       [20, 17, 15, 13, 10, 8, 5, 2, 0]
-//     ]
-//   }, {
-//     low: 0
-//   });
-
-//   // Let's put a sequence number aside so we can use it in the event callbacks
-//   var seq = 0,
-//     delays = 80,
-//     durations = 500;
-
-//   // Once the chart is fully created we reset the sequence
-//   chart.on('created', function() {
-//     seq = 0;
-//   });
-
-//   // On each drawn element by Chartist we use the Chartist.Svg API to trigger SMIL animations
-//   chart.on('draw', function(data) {
-//     seq++;
-
-//     if(data.type === 'line') {
-//       // If the drawn element is a line we do a simple opacity fade in. This could also be achieved using CSS3 animations.
-//       data.element.animate({
-//         opacity: {
-//           // The delay when we like to start the animation
-//           begin: seq * delays + 1000,
-//           // Duration of the animation
-//           dur: durations,
-//           // The value where the animation should start
-//           from: 0,
-//           // The value where it should end
-//           to: 1
-//         }
-//       });
-//     } else if(data.type === 'label' && data.axis === 'x') {
-//       data.element.animate({
-//         y: {
-//           begin: seq * delays,
-//           dur: durations,
-//           from: data.y + 100,
-//           to: data.y,
-//           // We can specify an easing function from Chartist.Svg.Easing
-//           easing: 'easeOutQuart'
-//         }
-//       });
-//     } else if(data.type === 'label' && data.axis === 'y') {
-//       data.element.animate({
-//         x: {
-//           begin: seq * delays,
-//           dur: durations,
-//           from: data.x - 100,
-//           to: data.x,
-//           easing: 'easeOutQuart'
-//         }
-//       });
-//     } else if(data.type === 'point') {
-//       data.element.animate({
-//         x1: {
-//           begin: seq * delays,
-//           dur: durations,
-//           from: data.x - 10,
-//           to: data.x,
-//           easing: 'easeOutQuart'
-//         },
-//         x2: {
-//           begin: seq * delays,
-//           dur: durations,
-//           from: data.x - 10,
-//           to: data.x,
-//           easing: 'easeOutQuart'
-//         },
-//         opacity: {
-//           begin: seq * delays,
-//           dur: durations,
-//           from: 0,
-//           to: 1,
-//           easing: 'easeOutQuart'
-//         }
-//       });
-//     } else if(data.type === 'grid') {
-//       // Using data.axis we get x or y which we can use to construct our animation definition objects
-//       var pos1Animation = {
-//         begin: seq * delays,
-//         dur: durations,
-//         from: data[data.axis.units.pos + '1'] - 30,
-//         to: data[data.axis.units.pos + '1'],
-//         easing: 'easeOutQuart'
-//       };
-
-//       var pos2Animation = {
-//         begin: seq * delays,
-//         dur: durations,
-//         from: data[data.axis.units.pos + '2'] - 100,
-//         to: data[data.axis.units.pos + '2'],
-//         easing: 'easeOutQuart'
-//       };
-
-//       var animations = {};
-//       animations[data.axis.units.pos + '1'] = pos1Animation;
-//       animations[data.axis.units.pos + '2'] = pos2Animation;
-//       animations['opacity'] = {
-//         begin: seq * delays,
-//         dur: durations,
-//         from: 0,
-//         to: 1,
-//         easing: 'easeOutQuart'
-//       };
-
-//       data.element.animate(animations);
-//     }
-//   });
-
-//   // For the sake of the example we update the chart every time it's created with a delay of 10 seconds
-//   chart.on('created', function() {
-//     if(window.__exampleAnimateTimeout) {
-//       clearTimeout(window.__exampleAnimateTimeout);
-//       window.__exampleAnimateTimeout = null;
-//     }
-//     window.__exampleAnimateTimeout = setTimeout(chart.update.bind(chart), 12000);
-//   });
-// };
-
-// // run();
-
-// var che = function(i) {
-//   alert(i);
-// }
--- ITClinic/project_list_script.js
+++ ITClinic/project_list_script.js
@@ -1,34 +1,50 @@
-
 var projects;
+var tags_for_bar = [];
 
-
+var count = 0;
 
 $(document).ready(function() {
     $.ajax({
     type: "GET",
-    url: "https://agile-depths-28542.herokuapp.com/api/projects?limit=5&offset=0&metrics=random", //, another", 
-    // url: "https://agile-depths-28542.herokuapp.com/api/projects/info",
+    url: "https://agile-depths-28542.herokuapp.com/api/public/projects?limit=10&offset=0&metrics=progress,countclosed",
     dataType: "json",
     headers: {
       'Authorization': '8006590e56914f96bb2a0d67a998a234'
     },
-  //   error: function(request, status, error) {
-  //     alert(request.status); // вот он код ответа
-  //  },
-    // beforeSend: function() {
-    //   alert('loading');
-    // },
     success: function(data) {
-        // data = JSON.parse(data);
-        // alert('Title ' + data[0].name + ' Client -  '  + data[0].client + ' repo ' + data[0].githubRepo);
         projects = data;
         preloader();
+        createTags(data);
         create(data);
-        // alert(data.active);
-        // alert(data);
     }
   });
 });
+
+var createTags = function(projects) {
+  var reset = document.querySelector('.reset');
+  reset.onclick = Reset;
+  for (let i = 0; i < projects.length; i++) {
+    for (let j = 0; j < projects[i].tags.length; j++) {
+      var element = projects[i].tags[j];
+      tags_for_bar.push(element);
+    }
+  }
+
+  tags_for_bar = new Set(tags_for_bar);
+  tags_for_bar = Array.from(tags_for_bar);
+  console.log(tags_for_bar);
+
+
+  var ulist = document.querySelector('.type-of-tags .first-type-of-tags');
+  for (let i = 0; i < tags_for_bar.length; i++) {
+    var lItem = document.createElement('li');
+    var liBtn = document.createElement('button');
+    liBtn.textContent = tags_for_bar[i];
+    liBtn.onclick = tags_listener;
+    lItem.appendChild(liBtn);
+    ulist.appendChild(lItem);
+  }
+};
 
 function preloader() {
   setInterval(() => {
@@ -42,9 +58,6 @@ function preloader() {
 
   }, 500);
 }
-
-
-// var button_id = 0;
 
 var create = function(data) {
   var boxList = document.querySelector('.list-of-projects');
@@ -64,14 +77,6 @@ var makeElement = function(tagName, className, text) {
   return element;
 };
 
-// function getCookie(name) {
-//   var matches = document.cookie.match(new RegExp(
-//     "(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"
-//   ));
-//   return matches ? decodeURIComponent(matches[1]) : undefined;
-// }
-
-
 var createBox = function(project) {
 
   var listItem = makeElement('li', 'project-box');
@@ -85,9 +90,7 @@ var createBox = function(project) {
   on_project_link.id = project.id;
   on_project_link.setAttribute('href', 'about_project.html');
   on_project_link.addEventListener('click', () => {
-    //alert(on_project_link.id);
     var str = 'id=' + on_project_link.id + '; path=/;';
-    // alert('str = ' + str);
     document.cookie = str;
     
   });
@@ -99,10 +102,9 @@ var createBox = function(project) {
   if (project.metrics.length == 0) {
     foo.textContent = '0' + '%';
   } else {
-    var status = Math.round(project.metrics[project.metrics.length - 1].value * 100);
+    var status = Math.round(project.metrics[0].value * 100);
     foo.textContent = status + '%';
   }
-  // foo.textContent = project.status_bar + '%';
 
   var st_bar = document.createElement('div');
   st_bar.classList.add('status-bar');
@@ -120,16 +122,6 @@ var createBox = function(project) {
   var tags = document.createElement('ul');
   tags.classList.add('topmenu-tags');
 
-
-  // var subUl = document.createElement('ul');
-  // subUl.classList.add('submenu-tags');
-  
-  // for (var i = 0; i < project.array_li_sublist.length; i++) {
-  //   var subtag = document.createElement('li');
-  //   subtag.textContent = project.array_li_sublist[i];
-  //   subUl.appendChild(subtag);
-  // }
-  
   if (project.tags.length == 0) {
     var tags_top = document.createElement('li');
     tags_top.textContent = 'Тегов нет :с';
@@ -138,13 +130,6 @@ var createBox = function(project) {
     for (var i = 0; i < project.tags.length; i++) {
       var tags_top = document.createElement('li');
       tags_top.textContent = project.tags[i];
-      // if (i === project.array_li.length - 1) {
-      //   tags_top.classList.add('others');
-      //   var fa_fa = document.createElement('i');
-      //   fa_fa.classList.add('fa');
-      //   tags_top.appendChild(fa_fa);
-      //   tags_top.appendChild(subUl);
-      // }
       tags.appendChild(tags_top);
     }
   }
@@ -153,28 +138,10 @@ var createBox = function(project) {
   divTags.appendChild(tags);
   listItem.appendChild(divTags);
 
-  // var button_id = project.id;
-
   return listItem;
 };
 
-// var boxList = document.querySelector('.list-of-projects');
-// for (var i = 0; i < projects.length; i++) {
-//   var boxItem = createBox(projects[i]);
-//   boxList.appendChild(boxItem);
-// }
-
-var count = 0;
-var count_projects = 0;
-var array_tags = [];
-var hidden_count = 0;
-// document.querySelectorAll('.project-box').forEach(element => {
-//   array_tags.push(element);
-// });
-
-document.querySelectorAll('.type-of-tags button').forEach((button) => {
-  button.addEventListener('click', () => {
-    // alert('ok');
+var tags_listener = function() {
     if (count < 3) {
       var reset = document.querySelector('.reset');
       reset.style.display = 'block';
@@ -185,10 +152,9 @@ document.querySelectorAll('.type-of-tags button').forEach((button) => {
         var tags = box.querySelectorAll('.topmenu-tags li');
         var flag = false;
         tags.forEach(tag => {
-          if (tag.textContent.toLowerCase() == button.textContent.toLowerCase()) {
+          if (tag.textContent.toLowerCase() == this.textContent.toLowerCase()) {
             flag = true;
             count_projects++;
-            // break;
           } 
         });
         if (flag == false) {
@@ -207,43 +173,28 @@ document.querySelectorAll('.type-of-tags button').forEach((button) => {
         }
       hidden_count = 0;
       
-      // if (count_projects == 0) {
-      //   document.querySelector('.message').style.display = 'block';
-      // }
-      
       var ull = document.querySelector('.selected-tags');
       var tag = document.createElement('div');
-      tag.textContent = button.textContent;
-      tag.style.backgroundColor = window.getComputedStyle( button , null).getPropertyValue('background-color');
+      tag.textContent = this.textContent;
+      tag.style.backgroundColor = window.getComputedStyle( this , null).getPropertyValue('background-color');
       tag.classList.add('pressed');
       ull.appendChild(tag);
-      button.disabled = true;
+      this.disabled = true;
 
     } else {
       alert('Максимум 3 тега!!!')
     }
-  });
-});
+}
 
-var reset = document.querySelector('.reset');
 
-// var Reset = function() {
-//   count = 0;
-//   reset.style.display = 'none';
-
-//   document.querySelectorAll('.project-box').forEach(elem => {
-//     elem.style.display = 'block';
-//   });
-
-//   document.querySelector('.message').style.display = 'none';
-// };
+var array_tags = [];
 
 var Reset = function() {
   document.querySelectorAll('.project-box').forEach(element => {
     array_tags.push(element);
   });
   count = 0;
-  reset.style.display = 'none';
+  this.style.display = 'none';
   document.querySelectorAll('.type-of-tags button').forEach(elem => {
     elem.disabled = false;
   });
@@ -262,29 +213,34 @@ var Reset = function() {
   });
 };
 
-reset.onclick = Reset;
+// document.querySelector('.search-button').addEventListener('click', () => {
+//   var s = document.querySelector('.search-form .search');
+//   if (s.value != '') {
+//     document.querySelectorAll('.project-box').forEach(box => {
+//       var tags = box.querySelectorAll('.topmenu-tags li');
+//       var flag = false;
+//       tags.forEach(tag => {
+//         if (tag.textContent.toLowerCase() == this.textContent.toLowerCase()) {
+//           flag = true;
+//           count_projects++;
+//         } 
+//       });
+//       if (flag == false) {
+//         box.style.display = 'none';
+//       }
+//     });
 
-// var on_project_button = document.querySelector('.on-project-page');
-// on_project_button.addEventListener('click', () => {
-//   alert('ok');
+//     var hidden_count = 0;
+//       document.querySelectorAll('.project-box').forEach(box => {
+//         if (box.style.display == 'none') {
+//           hidden_count++;
+//         }
+//       });
+//       if (hidden_count == projects.length) {
+//           document.querySelector('.message').style.display = 'block';
+//         }
+//       hidden_count = 0;
+
+//   }
 // });
 
-// on_project_button[0].addEventListener('click', () => {
-//   alert('hallo');
-// });
-
-// on_project_button.forEach((button) => {
-//   button.addEventListener('click', () => {
-//     //var i = button.id;
-//     alert('okay');
-//     // about_project_script.che(i);
-
-//   });
-// });
-
-
-
-// var check = function(id) {
-//   var d = document.querySelector('.status-project');
-//   alert(d);
-// };
